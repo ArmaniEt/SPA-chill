d3a4949b0abaf6ae33536721dd5253cf2cfb5905
armani <razzarioa@gmail.com>
2019-02-26 19:48:18 +0600

    Added .idea to gitignore, installed reactstrap with bootstrap, cleaned App.js, App.css

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+
+            </div>
+        );
+    }
 }
 
 export default App;
+
+// We have to have at least 3 components.
+// Create a main page with own layout. It will be in containers dir.
+// Try to use NavLink (read documentation).
+// Additionally components with layout to go to (I suppose it also will be in containers dir with own state each).
+// Also import BrowserRouter in App.js with other React Router components.
